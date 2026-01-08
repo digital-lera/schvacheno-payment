916551814c8d6bfb7e197021d3c055133c409caa
Valeria Strygina <valeria.strygina.home@gmail.com>
2026-01-08 20:58:12 +0300

    Updated projects and program files from template state

--- src/Services/Notifications/Program.cs
+++ src/Services/Notifications/Program.cs
@@ -1,2 +1,7 @@
-﻿
-  
+﻿Console.WriteLine("Schvacheno Notifications Service v1.0");
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddLogging(x => x.AddConsole());
+
+var app = builder.Build();
+await app.RunAsync();
--- src/Services/Payments/Program.cs
+++ src/Services/Payments/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Health checks для Docker
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy());
+
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+
+app.MapHealthChecks("/health");
+app.MapGet("/", () => "Schvacheno Payments Service v1.0");
+
 app.Run();
